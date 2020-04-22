9243c00bf07e85e68fdc2e144dde8237a7efa1cf
Ryo Tanaka <ryo_tanaka@tbadminnoMacBook-Pro.local>
2020-04-22 17:07:14 +0900

    整理

--- minecraft/memo/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import MinecraftMemo
-
-class MinecraftMemoForm(ModelForm):
-
-    class Meta:
-        model = MinecraftMemo
-        fields = ["title", "text", "coordinate_x", "coordinate_y", "coordinate_z"]
-        
